83d8e8ce88f5571cd404b82f164077ecce5ee933
gookbob <58072554+gookbob@users.noreply.github.com>
2022-06-10 17:05:49 +0900

    Update deezbot.py

--- deezbot.py
+++ deezbot.py
@@ -70,7 +70,7 @@ async def on_member_join(member):
     fmt = '{0.mention}님 {1.name}에 오신 것을 환영합니다. 왼쪽 메뉴 #🔪│입대신청을 눌러주세요.'.format(member, member.guild)
     channel = client.get_channel(696579283547848734)
     await channel.send(fmt)
-    fmt = '@everyone 새로운 전학생 {0.mention}님이 {1.name}에 입학하셨습니다. 환영해주세요.'.format(member, member.guild)
+    fmt = '@everyone {0.mention}님이 들어오셨습니다. 환영해주세요.'.format(member, member.guild)
     channel = client.get_channel(833412228530176100)
     await channel.send(fmt)
 
